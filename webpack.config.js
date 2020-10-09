dfbfae2941d5013fd22002827db4479ee3f61b57
yohn-cheung <62690502+yohn-cheung@users.noreply.github.com>
2020-10-09 09:20:28 +0200

    add dymanic imports code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -6,65 +6,68 @@ var fs = require("fs");
 const bundleOutputDir = './dist';
 
 module.exports = (env) => {
-    const isDevBuild = !(env && env.prod); 
+    const isDevBuild = !(env && env.prod);
 
     return [{
         devtool: 'source-map',
-        entry: './src/index.js',
+        entry: {
+          'widget': './src/index.js',
+        },
         output: {
-            filename: 'widget.js',
+            filename: '[name].js',
             path: path.resolve(bundleOutputDir),
         },
         devServer: {
             contentBase: bundleOutputDir
         },
         plugins: isDevBuild
-            ? [new webpack.SourceMapDevToolPlugin(), new copyWebpackPlugin({ patterns: [{ from: 'dev/'}]})]
+            ? [new webpack.SourceMapDevToolPlugin(), new copyWebpackPlugin({ patterns: [{ from: 'dev/' }] })]
             : [new webpack.BannerPlugin(fs.readFileSync('./LICENSE', 'utf8'))],
-            optimization: {
-                minimizer: true,
-                minimizer: [
-                    new TerserPlugin({
-                      cache: true,
-                      parallel: true,
-                      sourceMap: true, // Must be set to true if using source-maps in production
-                      terserOptions: {
+        optimization: {
+            minimizer: true,
+            minimizer: [
+                new TerserPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: true, // Must be set to true if using source-maps in production
+                    terserOptions: {
                         output: {
-                          comments: /(Simac|Triangle)/g,
+                            comments: /(Simac Triangle|Simac Triangle B.V.)/g,
                         },
-                      },
-                      extractComments: false
-                    }),
-                  ]
-            },
-            mode: isDevBuild ? 'development' : 'production',
-            module: {
-                rules: [
-                    { test: /\.html$/i, use: 'html-loader' },
-                    { test: /\.css$/i, use: ['style-loader', 'css-loader' + (isDevBuild ? '' : '?minimize')] },
-                    {
-                        test: /\.js$/i, exclude: /node_modules/, use: {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: [['@babel/env', {
-                                    'targets': {
-                                        'browsers': ['ie 6', 'safari 7']
-                                    }
-                                }]]
-                            }
-                        }
                     },
-                    { test: /\.s[ac]ss$/i, 
-											use: [
-                                                'style-loader',
-                                                'css-loader',
-                                                'sass-loader'
-											]
-							
-					},
-				    {test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,loader: 'file-loader?limit=100000'}
-                ]
-            }
+                    extractComments: false
+                }),
+            ]
+        },
+        mode: isDevBuild ? 'development' : 'production',
+        module: {
+            rules: [
+                { test: /\.html$/i, use: 'html-loader' },
+                { test: /\.css$/i, use: ['style-loader', 'css-loader' + (isDevBuild ? '' : '?minimize')] },
+                {
+                    test: /\.js$/i, exclude: /node_modules/, use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [['@babel/env', {
+                                'targets': {
+                                    'browsers': ['ie 6', 'safari 7']
+                                }
+                            }]]
+                        }
+                    }
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+
+                },
+                { test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/, loader: 'file-loader?limit=100000' }
+            ]
+        }
     }]
-    
+
 };
